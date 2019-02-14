578f1101a03bff6445a0abdd57faa2a0424d039a
galdinojr <galdinodbz@gmail.com>
2019-02-14 16:40:41 -0200

    teste activity

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -59,7 +59,7 @@ public class ToastyPlugin extends CordovaPlugin {
       _callbackContext = callbackContext;
 
       try{
-          Class<?> c = Class.forName("com.stanleyidesis.cordova.plugin.NewActivity");
+          Class<?> c = Class.forName("NewActivity");
           Intent intent=new Intent(context, c);
           cordova.getActivity().startActivity(intent);
           isOk = true;
