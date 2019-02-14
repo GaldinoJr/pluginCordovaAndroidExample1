98c3ada6bd2bc2794c53d2c75e241baf5ac3a87b
galdinojr <galdinodbz@gmail.com>
2019-02-14 16:35:22 -0200

    teste activity

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -2,72 +2,73 @@ package com.stanleyidesis.cordova.plugin;
 // The native Toast API
 import android.widget.Toast;
 // Cordova-required packages
+// import org.apache.cordova.CallbackContext;
+// import org.apache.cordova.CordovaPlugin;
+// import org.apache.cordova.PluginResult;
+// import org.json.JSONArray;
+// import org.json.JSONException;
+// import org.json.JSONObject;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.content.Context;
 import android.content.Intent;
-
-import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CordovaInterface;
-
 public class ToastyPlugin extends CordovaPlugin {
   private static final String DURATION_LONG = "long";
-
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-  }
-
   // @Override
-  // public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-  //     Context context = cordova.getActivity().getApplicationContext();
-  //     if(action.equals("new_activity")) {
-  //         this.openNewActivity(context);
-  //         return true;
+  // public boolean execute(String action, JSONArray args,
+  //   final CallbackContext callbackContext) {
+  //     // Verify that the user sent a 'show' action
+  //     if (!action.equals("show")) {
+  //       callbackContext.error("\"" + action + "\" is not a recognized action.");
+  //       return false;
   //     }
-  //     return false;
+  //     String message;
+  //     String duration;
+  //     try {
+  //       JSONObject options = args.getJSONObject(0);
+  //       message = options.getString("message");
+  //       duration = options.getString("duration");
+  //     } catch (JSONException e) {
+  //       callbackContext.error("Error encountered: " + e.getMessage());
+  //       return false;
+  //     }
+  //     // Create the toast
+  //     Toast toast = Toast.makeText(cordova.getActivity(), cordova.getActivity().getPackageName(),
+  //       DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+  //     // Display toast
+  //     toast.show();
+  //     // Send a positive result to the callbackContext
+  //     PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+
+  //     callbackContext.sendPluginResult(pluginResult);
+  //     return true;
   // }
 
-  private void openNewActivity(Context context) {
-      Intent intent = new Intent( "com.stanleyidesis.cordova.plugin.NewActivity");
-      // this.cordova.getActivity().startActivity(context, intent);
-      this.cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
+  private static CallbackContext _callbackContext;
+
+  public static CallbackContext getCallbackContext(){
+      return _callbackContext;
   }
 
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+      Context context=this.cordova.getActivity().getApplicationContext();
+      boolean isOk = false;
+      _callbackContext = callbackContext;
 
-  @Override
-  public boolean execute(String action, JSONArray args,
-    final CallbackContext callbackContext) {
-      // Verify that the user sent a 'show' action
-      // if (!action.equals("show")) {
-      //   callbackContext.error("\"" + action + "\" is not a recognized action.");
-      //   return false;
-      // }
-      // String message;
-      // String duration;
-      // try {
-      //   JSONObject options = args.getJSONObject(0);
-      //   message = options.getString("message");
-      //   duration = options.getString("duration");
-      // } catch (JSONException e) {
-      //   callbackContext.error("Error encountered: " + e.getMessage());
-      //   return false;
-      // }
-      // // Create the toast
-      // Toast toast = Toast.makeText(cordova.getActivity(), cordova.getActivity().getPackageName(),
-      //   DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
-      // // Display toast
-      // toast.show();
-      Context context = cordova.getActivity().getApplicationContext();
-      this.openNewActivity(context);
-      // Send a positive result to the callbackContext
-      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+      try{
+          Class<?> c = Class.forName("com.stanleyidesis.cordova.plugin.NewActivity");
+          Intent intent=new Intent(context, c);
+          cordova.getActivity().startActivity(intent);
+          isOk = true;
+      }catch(Exception e){
+          callbackContext.error(e.getMessage());
+          // DO nothing for while...
+      }
 
-      callbackContext.sendPluginResult(pluginResult);
-      return true;
+      return isOk;
   }
+
 }
